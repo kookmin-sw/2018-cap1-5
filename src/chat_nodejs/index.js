5e19fe1d2e4339709510483317c195186ab42f8d
hyungoo kang <33898835+hyungoo-kang-kmucs@users.noreply.github.com>
2018-05-25 22:23:40 +0900

    Update index.js
    RPS와 공통부분, 다른부분 추려냈음
    /*A-B*/ : 다른부분

--- src/chat_nodejs/index.js
+++ src/chat_nodejs/index.js
@@ -1,22 +1,47 @@
-var app = require('express')();
-var http = require('http').Server(app);
+// http, express, app,port 정의
+var http = require('http');
 var express = require('express');
-var io = require('socket.io')(http);
+var app = express();
 var port = process.env.PORT || 3000;
-var path = require('path');
+// createServer
+var server = http.createServer(app);//추가
+// socket.io 불러오기
+var socketio = require('socket.io');
+var io = socketio(server);
+/*A-B*/var clientPath = require('path');
+/*A-B*/console.log(`Serving static from ${clientPath}`);
 
-app.use(express.static(path.join(__dirname, 'js')));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
+// io.on 부분
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+
+
+
+
+
+
+
+
+
+
+
+
+  socket.on('message', function(msg){
+    io.emit('message', msg);
   });
 });
 
-http.listen(port, function(){
+// server.on listen
+server.on('error', (err) => {
+	console.error('Server error:', err);
+});
+server.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+
+/*A-B*/
+/*A-B*/app.use(express.static(clientPath.join(__dirname, 'js')));
+/*A-B*/app.get('/', function(req, res){
+/*A-B*/  res.sendFile(__dirname + '/index.html');
+/*A-B*/});
